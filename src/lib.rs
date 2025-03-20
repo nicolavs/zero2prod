a006c2a442e71f595f60e72909ac96088d3f4327
Nicola Vitaly <nicola.vitaly@bioeye.com>
2025-03-20 16:07:15 +1100

    ValidatedForm

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,62 @@
-use axum::http::StatusCode;
-use axum::{routing::get, Router};
+use axum::{
+    extract::{rejection::FormRejection, Form, FromRequest, Request},
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::get,
+    routing::post,
+    Router,
+};
+use serde::{de::DeserializeOwned, Deserialize};
+use thiserror::Error;
+use validator::Validate;
+#[derive(Debug, Deserialize, Validate)]
+struct FormData {
+    #[validate(length(min = 2, message = "Can not be empty"))]
+    email: String,
+
+    #[validate(length(min = 2, message = "Can not be empty"))]
+    name: String,
+}
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct ValidatedForm<T>(pub T);
+
+impl<T, S> FromRequest<S> for ValidatedForm<T>
+where
+    T: DeserializeOwned + Validate,
+    S: Send + Sync,
+    Form<T>: FromRequest<S, Rejection = FormRejection>,
+{
+    type Rejection = ServerError;
+
+    async fn from_request(req: Request, state: &S) -> Result<Self, Self::Rejection> {
+        let Form(value) = Form::<T>::from_request(req, state).await?;
+        value.validate()?;
+        Ok(ValidatedForm(value))
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ServerError {
+    #[error(transparent)]
+    ValidationError(#[from] validator::ValidationErrors),
+
+    #[error(transparent)]
+    AxumFormRejection(#[from] FormRejection),
+}
+
+impl IntoResponse for ServerError {
+    fn into_response(self) -> Response {
+        match self {
+            ServerError::ValidationError(_) => {
+                let message = format!("Input validation error: [{self}]").replace('\n', ", ");
+                (StatusCode::BAD_REQUEST, message)
+            }
+            ServerError::AxumFormRejection(_) => (StatusCode::BAD_REQUEST, self.to_string()),
+        }
+        .into_response()
+    }
+}
 
 use axum::extract::Path;
 
@@ -13,11 +70,17 @@ async fn health_check() -> StatusCode {
     StatusCode::OK
 }
 
+// Let's start simple: we always return a 200 OK
+async fn subscribe(ValidatedForm(_form_data): ValidatedForm<FormData>) -> StatusCode {
+    StatusCode::OK
+}
+
 fn app() -> Router {
     Router::new()
         .route("/{name}", get(greet))
         .route("/", get(|| async { "Hello, World!" }))
         .route("/health_check", get(health_check))
+        .route("/subscriptions", post(subscribe))
 }
 
 pub async fn run(listener: TcpListener) {
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,3 +1,16 @@
+async fn spawn_app() -> String {
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:0")
+        .await
+        .expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    tokio::spawn(async {
+        zero2prod::run(listener).await;
+    });
+    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
@@ -17,15 +30,50 @@ async fn health_check_works() {
     // Launch our application in the background ~somehow~
 }
 
-async fn spawn_app() -> String {
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:0")
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app().await;
+    let client = reqwest::Client::new();
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
         .await
-        .expect("Failed to bind random port");
-    // We retrieve the port assigned to us by the OS
-    let port = listener.local_addr().unwrap().port();
-    tokio::spawn(async {
-        zero2prod::run(listener).await;
-    });
-    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-    format!("http://127.0.0.1:{}", port)
+        .expect("Failed to execute request.");
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
